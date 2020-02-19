768324e386d33fb97dfeab7f7fe9035c083fb5a5
miles greene <miles.greene01@gmail.com>
2020-02-19 11:26:53 -0500

    ui_base functionality

--- core_ui.py
+++ core_ui.py
@@ -1,21 +1,27 @@
 import tkinter as tk
 from tkinter import messagebox
-import numpy as np
 import seabreeze
 from seabreeze.spectrometers import Spectrometer
 import matplotlib.pyplot as plt
 import matplotlib
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from testing_utils import generate_dummy_spectra
-from data_utils import get_plot
+import random
 
 root = tk.Tk()
 root.resizable(0, 0)
+root.title("Spectrometer Tool")
 matplotlib.use("TkAgg")
 
 device_name = tk.StringVar()
 device_name.set('No Device Detected')
 
+int_time_entry = tk.StringVar()
+int_time_entry.set("2")
+int_time = 2  # default integration time = 2us
+
+trigger_mode = tk.StringVar()
+trigger_mode.set("0")
 
 collect_control = True  # enable collection controls
 sample_control = True  # enable sampling controls
@@ -34,30 +40,92 @@ while not devices:
         test_mode = messagebox.askyesno("Use Test Data", "Use Random Test Data?\nSensor can be acquired at any point")
         break
 
-if test_mode:
-    fig = get_plot(device=None, test=True)
-elif not devices:
-    fig = get_plot(device=None, test=False)
-else:
-    spec = seabreeze.spectrometers.Spectrometer.from_first_available()
-    fig = get_plot(device=spec, test=False)
-canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.get_tk_widget().grid(row=1, column=0, columnspan=2)  # plot initial data
+fig = plt.Figure(figsize=(5, 5), dpi=100)
+spectra_plot = fig.add_subplot(111)
+spectra_plot.set_ylabel('Intensity')
+spectra_plot.set_xlabel('Wavelength [nm]')
+spectra_plot.set_title('Observed Emission Spectra')
 
 
 def update_plot():
     if not devices:
-        # messagebox.showerror('Error', "Error: No device detected")
-        f = get_plot(test=False)
-        cv = FigureCanvasTkAgg(fig, master=root)
-        cv.get_tk_widget().grid(row=1, column=0, columnspan=2)  # plot initial data
+        t_data = generate_dummy_spectra(central_spectra=(random.randint(300, 500), random.randint(500, 700),
+                                                         random.randint(700, 900)))
+        ref = messagebox.askyesno('Error', "Error: No device detected. Use Testing Data?")
+        if ref:  # refresh with sample data
+            spectra_plot.clear()
+            spectra_plot.set_ylabel('Intensity')
+            spectra_plot.set_xlabel('Wavelength [nm]')
+            spectra_plot.set_title('Observed Emission Spectra')
+            spectra_plot.plot(t_data[0], t_data[1])
+            canvas.draw()
     else:
-        pass
+        spectra_plot.clear()
+        spectra_plot.set_ylabel('Intensity')
+        spectra_plot.set_xlabel('Wavelength [nm]')
+        spectra_plot.set_title('Observed Emission Spectra')
+        spectra_plot.plot(spec.wavelengths(), spec.intensities())
+        canvas.draw()
 
+
+def reconnect_device():
+    global spec
+    if seabreeze.spectrometers.list_devices():
+        spec = seabreeze.spectrometers.Spectrometer.from_first_available()
+        device_name.set(spec.serial_number)
+    else:
+        messagebox.showerror("ERROR", "ERROR: No Device Detected")
+
+
+if test_mode:
+    tst_data = generate_dummy_spectra(central_spectra=(random.randint(300, 500), random.randint(500, 700),
+                                                       random.randint(700, 900)))
+    spectra_plot.plot(tst_data[0], tst_data[1])
+
+elif not devices:
+    spectra_plot.plot(0, 0)
+else:
+    spec = seabreeze.spectrometers.Spectrometer.from_first_available()
+    spectra_plot.plot(spec.wavelengths(), spec.intensities())
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.get_tk_widget().grid(row=1, column=0, columnspan=2, rowspan=20)  # plot initial data
 
 tk.Label(root, text="Connected Device:").grid(row=0, column=0)
 tk.Label(root, textvariable=device_name, bg="White", relief=tk.GROOVE).grid(row=0, column=1, sticky="NSEW")
 
-refresh = tk.Button(root, text="Refresh", command=update_plot)
-refresh.grid(row=1, column=2)
+reconnect = tk.Button(root, text="Reconnect Device", command=reconnect_device)
+reconnect.grid(row=0, column=2, columnspan=2, sticky="NSEW")
+
+tk.Label(text="Sampling Controls", relief=tk.GROOVE).grid(row=1, columnspan=2, column=2, sticky="NSEW")
+
+refresh = tk.Button(root, text="Refresh Data", command=update_plot)
+refresh.grid(row=2, column=2, columnspan=2, sticky="NSEW")
+
+tk.Label(root, text="Integration Time [μs]", relief=tk.GROOVE).grid(row=3, column=2, sticky="NSEW")
+int_entry = tk.Entry(textvariable=int_time_entry, relief=tk.FLAT, bg="white")
+int_entry.grid(row=3, column=3, sticky="NSEW")
+
+tk.Label(root, text="Trigger Mode", relief=tk.GROOVE).grid(row=4, column=2, sticky="NSEW")
+trigger_mode_entry = tk.Entry(root, textvariable=trigger_mode, relief=tk.FLAT, bg="white")
+trigger_mode_entry.grid(row=4, column=3, sticky="NSEW")
+
+
+def update_integration_time(a, b, c):
+    global int_time
+    if not int_time_entry:
+        int_entry.config(bg='red')
+    else:
+        try:
+            t = float(int_time_entry.get())
+            if t:
+                int_time = t
+                int_entry.config(bg="white")
+            else:
+                int_entry.config(bg="red")
+        except Exception:
+            int_entry.config(bg="red")
+
+
+int_time_entry.trace_variable('w', update_integration_time)
+
 root.mainloop()
