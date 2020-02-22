883f5ec640068ab17043d72e7179dadaf891530a
Miles Greene <mug298@psu.edu>
2020-02-22 13:10:22 -0500

    updated basic laser controls

--- core_ui.py
+++ core_ui.py
@@ -50,14 +50,48 @@ sync_fire_var = tk.IntVar()
 sync_fire_var.set(0)
 devices = seabreeze.spectrometers.list_devices()
 
+
+# laser control variables
 pulse_options = {'Continuous', 'Single Shot', 'Burst'}
 pulse_mode = tk.StringVar()
 pulse_mode.set('Continuous')
-
 rep_rate_control = tk.StringVar()
 rep_rate_control.set('10')
+diode_temp_var = tk.StringVar()
+diode_temp_var.set('N/A')
+auto_update = tk.IntVar()
+auto_update.set(0)
+power_options = {'Manual', 'Low Power'}
+power_mode = tk.StringVar()
+power_mode.set('Manual')
+auto_arm = tk.IntVar()
+auto_arm.set(0)
+burst_count_var = tk.StringVar()
+burst_count_var.set('1000')
+diode_current_var = tk.StringVar()
+diode_current_var.set(0.1)
+pulse_width_var = tk.StringVar()
+pulse_width_var.set('10')
+diode_trigger_options = {'Internal', 'External'}
+diode_trigger_var = tk.StringVar()
+diode_trigger_var.set('Internal')
 
-rep_rate_control
+
+# laser status vars
+fet_temp_var = tk.StringVar()
+fet_temp_var.set('N/A')
+bank_voltage_var = tk.StringVar()
+bank_voltage_var.set('N/A')
+safety_control = tk.IntVar()
+safety_control.set(0)
+bank_target_var = tk.StringVar()
+bank_target_var.set('N/A')
+fet_voltage_var = tk.StringVar()
+fet_voltage_var.set('N/A')
+fet_drop_var = tk.StringVar()
+fet_drop_var.set('N/A')
+fet_src_var = tk.StringVar()
+fet_src_var.set('N/A')
 
 fig = plt.Figure(figsize=(5, 5), dpi=100)
 spectra_plot = fig.add_subplot(111)
@@ -125,6 +159,14 @@ def reconnect_device():
         messagebox.showerror("ERROR", "ERROR: No Device Detected")
 
 
+def save_settings():
+    pass
+
+
+def load_settings():
+    pass
+
+
 def export_plot():
     name = filedialog.asksaveasfilename(initialdir="./",
                                         title="Select file",
@@ -159,7 +201,7 @@ else:
     spectra_plot.plot(emission_data.iloc[0:, 0], emission_data.iloc[0:, 1])
     device_name.set(spec.serial_number)
 canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.get_tk_widget().grid(row=1, column=0, columnspan=2, rowspan=20)  # plot initial data
+canvas.get_tk_widget().grid(row=1, column=0, columnspan=2, rowspan=16)  # plot initial data
 
 tk.Label(root, text="Connected Device:").grid(row=0, column=0)
 tk.Label(root, textvariable=device_name, bg="White", relief=tk.GROOVE).grid(row=0, column=1, sticky="NSEW")
@@ -188,13 +230,17 @@ tk.Checkbutton(root, text="Synchronize Laser", variable=sync_fire_var, relief=tk
 refresh = tk.Button(root, text="Acquire Sample", command=update_plot)
 refresh.grid(row=5, column=2, columnspan=2, sticky="NSEW")
 
-tk.Label(root, text="Current Settings", relief=tk.GROOVE).grid(row=6, column=2, columnspan=2, sticky="NSEW")
+tk.Label(root, text="Current Status", relief=tk.GROOVE).grid(row=6, column=2, columnspan=2, sticky="NSEW")
+
 tk.Label(root, text="Max Intensity", relief=tk.GROOVE).grid(row=7, column=2, sticky="NSEW")
 tk.Label(root, textvariable=max_intensity_var, bg='gray', relief=tk.FLAT).grid(row=7, column=3, sticky="NSEW")
+
 tk.Label(root, text="Integration Bounds", relief=tk.GROOVE).grid(row=8, column=2, sticky="NSEW")
 tk.Label(root, textvariable=integration_limits_var, bg='gray', relief=tk.FLAT).grid(row=8, column=3, sticky="NSEW")
+
 tk.Label(root, text="Pixel Count", relief=tk.GROOVE).grid(row=9, column=2, sticky="NSEW")
 tk.Label(root, textvariable=pixel_var, bg='gray', relief=tk.FLAT).grid(row=9, column=3, sticky="NSEW")
+
 tk.Label(root, text="Sample Count", relief=tk.GROOVE).grid(row=10, column=2, sticky="NSEW")
 tk.Label(root, textvariable=sample_var, bg='gray', relief=tk.FLAT).grid(row=10, column=3, sticky="NSEW")
 
@@ -204,9 +250,14 @@ img_button.grid(row=11, column=2, columnspan=2, sticky="NSEW")
 csv_button = tk.Button(root, text='Export CSV', command=export_csv)
 csv_button.grid(row=12, column=2, columnspan=2, sticky="NSEW")
 
+sav_button = tk.Button(root, text='Save Settings', command=save_settings)
+sav_button.grid(row=13, column=2, columnspan=2, sticky="NSEW")
+
+load_button = tk.Button(root, text='Load Settings', command=load_settings)
+load_button.grid(row=14, column=2, columnspan=2, sticky="NSEW")
 
 # laser control UI
-tk.Button(root, text=" ", state=tk.DISABLED).grid(row=0, column=4, rowspan=21, sticky="NSEW")  # divider
+tk.Button(root, text=" ", state=tk.DISABLED).grid(row=0, column=4, rowspan=18, sticky="NSEW")  # divider
 tk.Label(root, text="Laser Controls", relief=tk.FLAT).grid(row=0, column=5, columnspan=2, sticky="NSEW")
 
 tk.Label(text="Pulse Control", relief=tk.GROOVE).grid(row=1, columnspan=2, column=5, sticky="NSEW")
@@ -215,24 +266,25 @@ tk.Label(text="Pulse Mode", relief=tk.GROOVE).grid(row=2, column=5, sticky="NSEW
 tk.OptionMenu(root, pulse_mode, *pulse_options).grid(row=2, column=6, sticky="NSEW")
 
 tk.Label(text="Rep Rate", relief=tk.GROOVE).grid(row=3, column=5, sticky="NSEW")
-tk.Entry(relief=tk.FLAT, bg="white").grid(row=3, column=6, sticky="NSEW")
+tk.Entry(relief=tk.FLAT, bg="white", textvariable=rep_rate_control).grid(row=3, column=6, sticky="NSEW")
 
 tk.Label(text="Burst Count", relief=tk.GROOVE).grid(row=4, column=5, sticky="NSEW")
-tk.Entry(relief=tk.FLAT, bg="white").grid(row=4, column=6, sticky="NSEW")
+tk.Entry(relief=tk.FLAT, bg="white", textvariable=burst_count_var).grid(row=4, column=6, sticky="NSEW")
 
 tk.Label(text="Diode Settings", relief=tk.GROOVE).grid(row=5, column=5, columnspan=2, sticky="NSEW")
 
 tk.Label(text="Diode Current", relief=tk.GROOVE).grid(row=6, column=5, sticky="NSEW")
-tk.Entry(relief=tk.FLAT, bg="white").grid(row=6, column=6, sticky="NSEW")
+tk.Entry(relief=tk.FLAT, bg="white", textvariable=diode_current_var).grid(row=6, column=6, sticky="NSEW")
 
 tk.Label(text="Energy Mode", relief=tk.GROOVE).grid(row=7, column=5, sticky="NSEW")
-tk.Entry(relief=tk.FLAT, bg="white").grid(row=7, column=6, sticky="NSEW")
+tk.OptionMenu(root, power_mode, *power_options).grid(row=7, column=6, sticky="NSEW")
 
 tk.Label(text="Diode Pulse Width", relief=tk.GROOVE).grid(row=8, column=5, sticky="NSEW")
-tk.Entry(relief=tk.FLAT, bg="white").grid(row=8, column=6, sticky="NSEW")
+tk.Entry(relief=tk.FLAT, bg="white", textvariable=pulse_width_var).grid(row=8, column=6, sticky="NSEW")
 
 tk.Label(text="Diode Trigger", relief=tk.GROOVE).grid(row=9, column=5, sticky="NSEW")
-tk.Entry(relief=tk.FLAT, bg="white").grid(row=9, column=6, sticky="NSEW")
+tk.OptionMenu(root, diode_trigger_var, *diode_trigger_options).grid(row=9, column=6, sticky="NSEW")
+tk.OptionMenu(root, diode_trigger_var, *diode_trigger_options).grid(row=9, column=6, sticky="NSEW")
 
 tk.Label(text="Laser Controls", relief=tk.GROOVE).grid(row=10, column=5, columnspan=2, sticky="NSEW")
 
@@ -244,35 +296,25 @@ tk.Label(text="Laser Status", relief=tk.GROOVE).grid(row=12, column=5, sticky="N
 laser_status = tk.Label(text='', relief=tk.FLAT, bg='gray')
 laser_status.grid(row=12, column=6, sticky="NSEW")
 
-auto_arm = tk.IntVar()
-auto_arm.set(0)
 tk.Checkbutton(root, text="Auto Arm", variable=auto_arm, relief=tk.FLAT)\
     .grid(row=13, column=5, sticky="NSEW")
 
-auto_update = tk.IntVar()
-auto_update.set(0)
 tk.Checkbutton(root, text="Auto Update Data", variable=auto_update, relief=tk.FLAT)\
     .grid(row=13, column=6, sticky="NSEW")
 
-safety_control = tk.IntVar()
-safety_control.set(0)
 tk.Checkbutton(root, text="Disable Safety", variable=safety_control, relief=tk.FLAT)\
     .grid(row=14, column=5, sticky="NSEW")
 
 fire_control = tk.Button(text='Fire', bg='red')
-fire_control.grid(row=13, column=7, columnspan=2, rowspan=8, sticky="NSEW")
+fire_control.grid(row=13, column=7, columnspan=2, rowspan=7, sticky="NSEW")
 
 tk.Label(root, text="Laser Status", relief=tk.FLAT).grid(row=0, column=7, columnspan=2, sticky="NSEW")
 
 tk.Label(root, text="Thermals", relief=tk.GROOVE).grid(row=1, column=7, columnspan=2, sticky="NSEW")
 
-diode_temp_var = tk.StringVar()
-diode_temp_var.set('N/A')
 tk.Label(root, text="Diode [°C]", relief=tk.GROOVE).grid(row=2, column=7, sticky="NSEW")
 tk.Label(root, textvariable=diode_temp_var, bg="Gray", relief=tk.FLAT).grid(row=2, column=8, sticky="NSEW")
 
-fet_temp_var = tk.StringVar()
-fet_temp_var.set('N/A')
 tk.Label(root, text="FET [°C]", relief=tk.GROOVE).grid(row=3, column=7, sticky="NSEW")
 tk.Label(root, textvariable=fet_temp_var, bg="Gray", relief=tk.FLAT).grid(row=3, column=8, sticky="NSEW")
 
@@ -288,23 +330,15 @@ tk.Label(root, textvariable=heater_temp_var, bg="Gray", relief=tk.FLAT).grid(row
 
 tk.Label(root, text="Voltage", relief=tk.GROOVE).grid(row=6, column=7, columnspan=2, sticky="NSEW")
 
-bank_voltage_var = tk.StringVar()
-bank_voltage_var.set('N/A')
 tk.Label(root, text="Bank Voltage", relief=tk.GROOVE).grid(row=7, column=7, sticky="NSEW")
 tk.Label(root, textvariable=bank_voltage_var, bg="Gray", relief=tk.FLAT).grid(row=7, column=8, sticky="NSEW")
 
-bank_target_var = tk.StringVar()
-bank_target_var.set('N/A')
 tk.Label(root, text="Target Bank Voltage", relief=tk.GROOVE).grid(row=8, column=7, sticky="NSEW")
 tk.Label(root, textvariable=bank_target_var, bg="Gray", relief=tk.FLAT).grid(row=8, column=8, sticky="NSEW")
 
-fet_voltage_var = tk.StringVar()
-fet_voltage_var.set('N/A')
 tk.Label(root, text="FET Voltage", relief=tk.GROOVE).grid(row=9, column=7, sticky="NSEW")
 tk.Label(root, textvariable=fet_voltage_var, bg="Gray", relief=tk.FLAT).grid(row=9, column=8, sticky="NSEW")
 
-fet_drop_var = tk.StringVar()
-fet_drop_var.set('N/A')
 tk.Label(root, text="FET Drop", relief=tk.GROOVE).grid(row=10, column=7, sticky="NSEW")
 tk.Label(root, textvariable=fet_drop_var, bg="Gray", relief=tk.FLAT).grid(row=10, column=8, sticky="NSEW")
 
@@ -313,8 +347,6 @@ fet_drain_var.set('N/A')
 tk.Label(root, text="FET Drain", relief=tk.GROOVE).grid(row=11, column=7, sticky="NSEW")
 tk.Label(root, textvariable=fet_drain_var, bg="Gray", relief=tk.FLAT).grid(row=11, column=8, sticky="NSEW")
 
-fet_src_var = tk.StringVar()
-fet_src_var.set('N/A')
 tk.Label(root, text="FET Source", relief=tk.GROOVE).grid(row=12, column=7, sticky="NSEW")
 tk.Label(root, textvariable=fet_src_var, bg="Gray", relief=tk.FLAT, width=10).grid(row=12, column=8, sticky="NSEW")
 
